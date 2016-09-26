da6786658bdf3ef67056291f698687a6664af7a6
Marten <krings.marten@gmail.com>
2016-09-26 17:00:59 +0200

    setup

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 /**
  * Created by Marten on 9/26/2016.
  */
+var express = require('express');
+var app = express();
+
+//parse application/json
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
